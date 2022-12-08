b9431f3a1d5b2e36e33e888a09504980fd53db91
MMuneebZaidi <119360402+MMuneebZaidi@users.noreply.github.com>
2022-12-09 00:27:41 +0500

    Find Controller and Frontend work

--- RideYourStyle/src/main/java/codes/rideyourstyle/FindCarController.java
+++ RideYourStyle/src/main/java/codes/rideyourstyle/FindCarController.java
@@ -8,11 +8,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FindCarController implements Initializable {
     @FXML
@@ -26,22 +33,59 @@ public class FindCarController implements Initializable {
     @FXML
     private ChoiceBox<String> Engine ;
     @FXML
-    private ChoiceBox<String> Mileage ;
-    @FXML
-    private ChoiceBox<String> Price ;
-    @FXML
     private ChoiceBox<String> BodyType ;
+    @FXML
+    private Slider minPriceSlider;
+    @FXML
+    private Slider maxPriceSlider;
+    @FXML
+    private Label minPriceLabel;
+    @FXML
+    private Label maxPriceLabel;
+    @FXML
+    private ListView<String> vehicleListView;
+
+
+    int minPrice;
+    int maxPrice;
+    ObservableList<String> allVehiclesName = FXCollections.observableArrayList();
+
+    String[] engineRanges = {"1500 - 2999","3000 - 4499","4500 - 5999","6000 - 7499"};
+    String[] bodyType = {"Sadan","SUV","Coupe"};
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         DatabaseConnection connectNow = new DatabaseConnection();
         Connection connectDB = connectNow.getDatabaseLink();
-        String[] engineRanges = {"1500 - 2990","3000 - 4490","4500 - 5990","6000 - 7490"};
-        String[] bodyType = {"Sadan","SUV","Coupe"};
-
+        Engine.getItems().addAll(engineRanges);
+        BodyType.getItems().addAll(bodyType);
+        try{
+            Statement stm = connectDB.createStatement();
+            String query =  "(select name from bentley)union" +
+                            "(select name from bmw)union" +
+                            "(select name from chevrolet)union" +
+                            "(select name from mercedes)union" +
+                            "(select name from porsche)union" +
+                            "(select name from rollsroyce)";
+            ResultSet rs = stm.executeQuery(query);
+            while (rs.next()){
+                String carName = rs.getString("name");
+                allVehiclesName.add(carName);
+            }
+        }catch (Exception e){
+            Logger.getLogger(FindCarController.class.getName()).log(Level.SEVERE,null,e);
+        }
+        vehicleListView.getItems().addAll(allVehiclesName);
+        minPriceSlider.valueProperty().addListener((observableValue, number, t1) -> {
+            minPrice = (int) minPriceSlider.getValue();
+            minPriceLabel.setText(Integer.toString(minPrice));
+        });
+        maxPriceSlider.valueProperty().addListener((observableValue, number, t1) -> {
+            maxPrice = (int) maxPriceSlider.getValue();
+            maxPriceLabel.setText(Integer.toString(maxPrice));
+        });
     }
-
-    ObservableList<Vehicle> allVehicles = FXCollections.observableArrayList();
-
-
 }
+
+
+
