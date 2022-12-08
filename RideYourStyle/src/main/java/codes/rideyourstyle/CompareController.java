613627d1c831a28aec210e21bd2115a2f90d78c3
Yan309 <FA21-BCS-105@cuilahore.edu.pk>
2022-12-08 22:34:04 +0500

    Compare update

--- RideYourStyle/src/main/java/codes/rideyourstyle/CompareController.java
+++ RideYourStyle/src/main/java/codes/rideyourstyle/CompareController.java
@@ -1,8 +1,13 @@
 package codes.rideyourstyle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -18,6 +23,16 @@ public class CompareController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        String[] arr ={"Bentley Bentayga","Bentley Continental GT","Bentley Flying Spur","Bentley Mulsanne","BMW 740i","BMW m850i","BMW X7","Chevrolet"};
+        String[] arr ={"Bentley Bentayga","Bentley Continental GT","Bentley Flying Spur","Bentley Mulsanne","BMW 740i","BMW m850i","BMW X7","Chevrolet "};
+        choicebox1.getItems().addAll(arr);
     }
+    @FXML
+    void backButton(ActionEvent ev) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(RideYouStyle.class.getResource("Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        Stage stage = (Stage) (((Node)ev.getSource()).getScene().getWindow());
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
