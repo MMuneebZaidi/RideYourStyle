483479b17980f3483a7bd6fa485a4b7e11a1e16a
Yan309 <FA21-BCS-105@cuilahore.edu.pk>
2022-12-08 20:39:09 +0500

    Compare update

--- RideYourStyle/src/main/java/codes/rideyourstyle/CompareController.java
+++ RideYourStyle/src/main/java/codes/rideyourstyle/CompareController.java
@@ -1,4 +1,23 @@
 package codes.rideyourstyle;
 
-public class CompareController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CompareController implements Initializable {
+
+    @FXML
+    private ChoiceBox<String> choicebox1;
+
+    @FXML
+    private ChoiceBox<String> choicebox2;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String[] arr ={"Bentley Bentayga","Bentley Continental GT","Bentley Flying Spur","Bentley Mulsanne","BMW 740i","BMW m850i","BMW X7","Chevrolet"};
+    }
 }
