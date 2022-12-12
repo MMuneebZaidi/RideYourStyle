73f683ff361ba8569113983bd1d854b01803179d
Yan309 <FA21-BCS-105@cuilahore.edu.pk>
2022-12-12 19:32:27 +0500

    something new (database)

--- RideYourStyle/src/main/java/codes/rideyourstyle/ComparedScene.java
+++ RideYourStyle/src/main/java/codes/rideyourstyle/ComparedScene.java
@@ -45,6 +45,5 @@ public class ComparedScene implements Initializable {
             }
             car1list.getItems().addAll(car1.name,car1.engine, car1.transmissionType,car1.enginePower,car1.topSpeed,car1.acceleration,car1.mileage,car1.fuelType,car1.fuelCapacity,car1.bodyType,car1.seatingCapacity,car1.doors,car1.wheelSize,car1.convertible,car1.rating,car1.price,car1.stock);
             car2list.getItems().addAll(car2.name,car2.engine, car2.transmissionType,car2.enginePower,car2.topSpeed,car2.acceleration,car2.mileage,car2.fuelType,car2.fuelCapacity,car2.bodyType,car2.seatingCapacity,car2.doors,car2.wheelSize,car2.convertible,car2.rating,car2.price,car2.stock);
-
     }
 }
