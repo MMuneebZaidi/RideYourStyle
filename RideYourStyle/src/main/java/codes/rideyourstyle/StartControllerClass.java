86858e07689df73382cf53b26ec3d3a632a01afd
MMuneebZaidi <119360402+MMuneebZaidi@users.noreply.github.com>
2022-12-17 17:36:24 +0500

    Best Push Ever

--- RideYourStyle/src/main/java/codes/rideyourstyle/StartControllerClass.java
+++ RideYourStyle/src/main/java/codes/rideyourstyle/StartControllerClass.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class StartControllerClass {
