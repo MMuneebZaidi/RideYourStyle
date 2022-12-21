901ecd8d0ed083ad60821c6a51106dd691d1f47c
MMuneebZaidi <119360402+MMuneebZaidi@users.noreply.github.com>
2022-12-21 17:19:16 +0500

    Delete RideYourStyle directory

--- RideYourStyle/src/main/java/codes/rideyourstyle/RollsRoyce.java
+++
@@ -1,9 +0,0 @@
-package codes.rideyourstyle;
-
-import java.sql.Blob;
-
-public class RollsRoyce extends Vehicle{
-    public RollsRoyce(String name, String engine, String transmissionType, String enginePower, String topSpeed, String acceleration, String mileage, String fuelType, String bodyType, String price, String rating, String seatingCapacity, String convertible, String doors, String wheelSize, String fuelCapacity, String stock, Blob image) {
-        super(name, engine, transmissionType, enginePower, topSpeed, acceleration, mileage, fuelType, bodyType, price, rating, seatingCapacity, convertible, doors, wheelSize, fuelCapacity, stock, image);
-    }
-}
