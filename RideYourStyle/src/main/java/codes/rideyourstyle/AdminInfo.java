a41cf7ec3fc4c0f0f307506250df26673b341fd7
Yan309 <119399404+Yan309@users.noreply.github.com>
2022-12-24 12:50:08 +0500

    Delete RideYourStyle directory

--- RideYourStyle/src/main/java/codes/rideyourstyle/AdminInfo.java
+++
@@ -1,8 +0,0 @@
-package codes.rideyourstyle;
-
-public class AdminInfo extends Info {
-
-    public AdminInfo(String name, String username, String email, String password, String phoneNumber, int age, String CNIC, String city) {
-        super(name, username, email, password, phoneNumber, age, CNIC, city);
-    }
-}
