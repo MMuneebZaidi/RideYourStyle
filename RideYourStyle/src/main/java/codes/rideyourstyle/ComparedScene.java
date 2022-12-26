64270c9c45ed02bbf355c21c2ce69b0d0b56b2e5
MMuneebZaidi <119360402+MMuneebZaidi@users.noreply.github.com>
2022-12-26 20:42:07 +0500

    Many things

--- RideYourStyle/src/main/java/codes/rideyourstyle/ComparedScene.java
+++ RideYourStyle/src/main/java/codes/rideyourstyle/ComparedScene.java
@@ -57,7 +57,7 @@ public class ComparedScene implements Initializable {
     private ImageView compimg2;
 
     ObservableList<Vehicle> list = FXCollections.observableArrayList();
-    Vehicle car1 , car2;
+    static Vehicle car1 , car2;
     DataSingleton data = DataSingleton.getInstance();
 
 
@@ -77,7 +77,6 @@ public class ComparedScene implements Initializable {
         }
         @Override
         protected void updateItem(String item, boolean empty) {
-
             setText(null);  // No text in label of super class
             if (empty) {
                 lastItem = null;
@@ -85,104 +84,80 @@ public class ComparedScene implements Initializable {
             } else {
                 lastItem = item;
                 label.setText(item!=null ? item : "<null>");
-                DataSingleton data = DataSingleton.getInstance();
-                String s1 = data.getCar1();
-                String s2 = data.getCar2();
-                ObservableList<Vehicle> list = RideYouStyle.allVehicles;
-                Vehicle car1 = null;
-                Vehicle car2 = null;
-                for (Vehicle vehicle : list) {
-                    if (vehicle.name.equals(s1))
-                        car1 = vehicle;
-                    else if (vehicle.name.equals(s2))
-                        car2 = vehicle;
-            }
-                assert car1 != null;
-                assert car2 != null;
-                String[] ar1 = {car1.name,car1.transmissionType,car1.fuelType,car1.bodyType,car1.wheelSize,car1.convertible,car1.price,car1.stock,car2.name,
-                car2.transmissionType,car2.fuelType,car2.bodyType,car2.wheelSize,car2.convertible,car2.price,car2.stock};
-            for (String s : ar1) {
-                if (lastItem.equals(s)) {
+                String[] ar1 = {car1.name,car1.transmissionType,car1.fuelType,car1.bodyType,car1.wheelSize,car1.convertible,car1.price,car1.stock,car1.doors
+                        ,car2.name, car2.transmissionType,car2.fuelType,car2.bodyType,car2.wheelSize,car2.convertible,car2.price,car2.stock,car2.doors};
+                for (String s : ar1) {
+                    if (lastItem.equals(s)) {
                     arrow1.setVisible(false);
-                }
-            }
-            String[] ar2 = {car1.engine,car1.enginePower,car1.topSpeed,car1.acceleration,car1.mileage,car1.fuelCapacity,car1.seatingCapacity,car1.doors,car1.rating};
-            String[] ar3 = {car2.engine,car2.enginePower,car2.topSpeed,car2.acceleration,car2.mileage,car2.fuelCapacity,car2.seatingCapacity,car2.doors,car2.rating};
-            for(int i = 0; i<ar2.length;i++){
-                if(lastItem.equals(ar2[i])){
-                    if(lastItem.equals(car1.mileage)){
-                        String[] splitCar1 = ar2[i].split(" ");
-                        String[] splitCar2 = ar3[i].split(" ");
-                        double avCar1 = ((Double.parseDouble(splitCar1[0])+Double.parseDouble(splitCar1[2]))*0.5);
-                        double avCar2 = ((Double.parseDouble(splitCar2[0])+Double.parseDouble(splitCar2[2]))*0.5);
-                        if(avCar1<avCar2){
-                            arrow1.setRotate(180);
-                            arrow1.setFill(Color.RED);
-
-                        } else if (avCar1==avCar2) {
-                            arrow1.setVisible(false);
-                        }
-                        continue;
-                    }
-                    if(lastItem.equals(car1.acceleration)){
-                        String[] splitCar1 = ar2[i].split("s");
-                        String[] splitCar2 = ar3[i].split("s");
-                        String car1acc = splitCar1[0];
-                        String car2acc = splitCar2[0];
-                        if(car1acc.equals(car2acc)){
-                            arrow1.setVisible(false);
-                        }
-                        else if(Double.parseDouble(car1acc) >Double.parseDouble(car2acc)) {
-                            arrow1.setRotate(180);
-                            arrow1.setFill(Color.RED);
-
-                        }
-                        continue;
-                    }
-                    if(lastItem.equals(car1.seatingCapacity)){
-                        String c1=ar2[i];
-                        String c2 = ar3[i];
-                        if(c1.equals(c2)){
-                            arrow1.setVisible(false);
-                        }
-                        else if((int)Double.parseDouble(c1) <(int)Double.parseDouble(c2)) {
-                            arrow1.setRotate(180);
-                            arrow1.setFill(Color.RED);
-                        }
-                        continue;
-                    }
-                    if(lastItem.equals(car1.rating)){
-                        if(ar2[i].equals(ar3[i])){
-                            arrow1.setVisible(false);
-                        }
-                        else if(Double.parseDouble(ar2[i]) <Double.parseDouble(ar3[i])) {
-                            arrow1.setRotate(180);
-                            arrow1.setFill(Color.RED);
-
-                        }
-                        continue;
-                    }
-                    if(lastItem.equals(car1.price)){
-                        arrow1.setVisible(false);
-                        continue;
-                    }
-                    int i1 = ar2[i].compareTo(ar3[i]);
-                    if(i1 ==0){
-                        arrow1.setVisible(false);
-                    }
-                    else if(i1 <0) {
-                        arrow1.setRotate(180);
-                        arrow1.setFill(Color.RED);
-
                     }
                 }
-            }
+                String[] ar2 = {car1.engine,car1.enginePower,car1.topSpeed,car1.acceleration,car1.mileage,car1.fuelCapacity,car1.seatingCapacity,car1.rating};
+                String[] ar3 = {car2.engine,car2.enginePower,car2.topSpeed,car2.acceleration,car2.mileage,car2.fuelCapacity,car2.seatingCapacity,car2.rating};
+                for(int i = 0; i<ar2.length;i++){
+                    if(lastItem.equals(ar2[i])){
+                        if(lastItem.equals(car1.mileage)){
+                            String[] splitCar1 = ar2[i].split(" ");
+                            String[] splitCar2 = ar3[i].split(" ");
+                            double avCar1 = ((Double.parseDouble(splitCar1[0])+Double.parseDouble(splitCar1[2]))*0.5);
+                            double avCar2 = ((Double.parseDouble(splitCar2[0])+Double.parseDouble(splitCar2[2]))*0.5);
+                            if(avCar1<avCar2){
+                                arrow1.setRotate(180);
+                                arrow1.setFill(Color.RED);
+
+                            } else if (avCar1==avCar2) {
+                                arrow1.setVisible(false);
+                            }
+                            continue;
+                        }
+                        if(lastItem.equals(car1.acceleration)){
+                            String[] splitCar1 = ar2[i].split("s");
+                            String[] splitCar2 = ar3[i].split("s");
+                            String car1acc = splitCar1[0];
+                            String car2acc = splitCar2[0];
+                            if(car1acc.equals(car2acc)){
+                                arrow1.setVisible(false);
+                            }
+                            else if(Double.parseDouble(car1acc) >Double.parseDouble(car2acc)) {
+                                arrow1.setRotate(180);
+                                arrow1.setFill(Color.RED);
+                            }
+                            continue;
+                        }
+                        if(lastItem.equals(car1.seatingCapacity)){
+                            String c1=ar2[i];
+                            String c2 = ar3[i];
+                            if(c1.equals(c2)){
+                                arrow1.setVisible(false);
+                            }
+                            else if((int)Double.parseDouble(c1) <(int)Double.parseDouble(c2)) {
+                                arrow1.setRotate(180);
+                                arrow1.setFill(Color.RED);
+                            }
+                            continue;
+                        }
+                        if(lastItem.equals(car1.rating)){
+                            if(ar2[i].equals(ar3[i])){
+                                arrow1.setVisible(false);
+                            }
+                            else if(Double.parseDouble(ar2[i]) <Double.parseDouble(ar3[i])) {
+                                arrow1.setRotate(180);
+                                arrow1.setFill(Color.RED);
+                            }
+                            continue;
+                        }
+                        int i1 = ar2[i].compareTo(ar3[i]);
+                        if(i1 ==0){
+                            arrow1.setVisible(false);
+                        }
+                        else if(i1 <0) {
+                            arrow1.setRotate(180);
+                            arrow1.setFill(Color.RED);
+                        }
+                    }
+                }
             setGraphic(hbox);
             }
-
         }
-
-
     }
     static class XCell2 extends ListCell<String> {
         HBox hbox = new HBox();
@@ -200,7 +175,6 @@ public class ComparedScene implements Initializable {
         }
         @Override
         protected void updateItem(String item, boolean empty) {
-
             setText(null);  // No text in label of super class
             if (empty) {
                 lastItem = null;
@@ -208,29 +182,15 @@ public class ComparedScene implements Initializable {
             } else {
                 lastItem = item;
                 label.setText(item!=null ? item : "<null>");
-                DataSingleton data = DataSingleton.getInstance();
-                String s1 = data.getCar1();
-                String s2 = data.getCar2();
-                ObservableList<Vehicle> list = RideYouStyle.allVehicles;
-                Vehicle car1 = null;
-                Vehicle car2 = null;
-                for (Vehicle vehicle : list) {
-                    if (vehicle.name.equals(s1))
-                        car1 = vehicle;
-                    else if (vehicle.name.equals(s2))
-                        car2 = vehicle;
-                }
-                assert car1 != null;
-                assert car2 != null;
-                String[] ar1 = {car1.name,car1.transmissionType,car1.fuelType,car1.bodyType,car1.wheelSize,car1.convertible,car1.price,car1.stock,car2.name,
-                        car2.transmissionType,car2.fuelType,car2.bodyType,car2.wheelSize,car2.convertible,car2.price,car2.stock};
+                String[] ar1 = {car1.name,car1.transmissionType,car1.fuelType,car1.bodyType,car1.wheelSize,car1.convertible,car1.price,car1.stock,car1.doors
+                        ,car2.name,car2.transmissionType,car2.fuelType,car2.bodyType,car2.wheelSize,car2.convertible,car2.price,car2.stock,car2.doors};
                 for (String s : ar1) {
                     if (lastItem.equals(s)) {
                         arrow1.setVisible(false);
                     }
                 }
-                String[] ar2 = {car1.engine,car1.enginePower,car1.topSpeed,car1.acceleration,car1.mileage,car1.fuelCapacity,car1.seatingCapacity,car1.doors,car1.rating};
-                String[] ar3 = {car2.engine,car2.enginePower,car2.topSpeed,car2.acceleration,car2.mileage,car2.fuelCapacity,car2.seatingCapacity,car2.doors,car2.rating};
+                String[] ar2 = {car1.engine,car1.enginePower,car1.topSpeed,car1.acceleration,car1.mileage,car1.fuelCapacity,car1.seatingCapacity,car1.rating};
+                String[] ar3 = {car2.engine,car2.enginePower,car2.topSpeed,car2.acceleration,car2.mileage,car2.fuelCapacity,car2.seatingCapacity,car2.rating};
                 for (int i = 0;i< ar3.length;i++){
                     if(lastItem.equals(ar3[i])){
                         if(lastItem.equals(car2.mileage)){
@@ -241,7 +201,6 @@ public class ComparedScene implements Initializable {
                             if(avCar1<avCar2){
                                 arrow1.setRotate(180);
                                 arrow1.setFill(Color.RED);
-
                             } else if (avCar1==avCar2) {
                                 arrow1.setVisible(false);
                             }
@@ -283,10 +242,6 @@ public class ComparedScene implements Initializable {
                                 arrow1.setFill(Color.RED);
 
                             }
-                            continue;
-                        }
-                        if(lastItem.equals(car2.price)){
-                            arrow1.setVisible(false);
                             continue;
                         }
                         int i1 = ar3[i].compareTo(ar2[i]);
