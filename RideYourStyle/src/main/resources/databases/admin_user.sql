1ea60b47d24cae2b6f024a7b046df30722e06280
ReyHaider <fa21-bcs-054@cuilahore.edu.pk>
2022-12-27 14:44:11 +0500

    ok

--- RideYourStyle/src/main/resources/databases/admin_user.sql
+++ RideYourStyle/src/main/resources/databases/admin_user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 27, 2022 at 09:44 AM
+-- Generation Time: Dec 25, 2022 at 02:25 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -30,21 +30,13 @@ SET time_zone = "+00:00";
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `Name` varchar(50) NOT NULL,
-  `Username` varchar(25) NOT NULL,
   `Email` varchar(50) NOT NULL,
   `Password` varchar(50) NOT NULL,
-  `CNIC` varchar(13) NOT NULL,
-  `Phone Number` varchar(11) NOT NULL,
+  `CNIC` int(13) NOT NULL,
+  `Phone Number` int(11) NOT NULL,
   `Age` int(2) NOT NULL,
   `City` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `Name`, `Username`, `Email`, `Password`, `CNIC`, `Phone Number`, `Age`, `City`) VALUES
-(1, 'Muhammad Muneeb Zaidi', 'Muneeb', 'fa21-bcs-036@cuilahore.edu.pk', 'dsaqw_2018', '3520262361115', '03189464829', 19, 'Lahore');
 
 -- --------------------------------------------------------
 
@@ -69,9 +61,12 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `Name`, `Username`, `Email`, `Password`, `CNIC`, `Phone Number`, `Age`, `City`) VALUES
-(2, 'Muhammad Muneeb Zaidi', 'muneebzaidi', 'muneebzaidi123@gmail.com', '26Sep2003', '3520262361115', '03189464829', 20, 'Lahore'),
-(9, 'Muneeb', 'muneebzaiodi', 'muneebzaidi@gmail.com', 'okMuneeb', '3892389283123', '03189464829', 19, 'Lahore'),
-(10, 'Test', 'testing', 'test@test.test', 'Testing123', '3520212345678', '03001234567', 19, 'Lahore');
+(1, 'Muneeb', 'muneebzaidi', 'muneebzaidi12345@gmail.com', 'okMuneeb', '3520262361115', '3189464829', 19, 'Lahore'),
+(2, 'Muhammad Muneeb Zaidi', 'muneebzaidi', 'muneebzaidi123@gmail.com', 'MuneebZaidi123', '3520262361115', '03189464829', 20, 'Lahore'),
+(6, 'Ali', 'alihamza', 'ali@gmail.com', 'okMuneeb', '3274613431123', '03138278944', 23, 'Lahore'),
+(7, 'Test Unit', 'test', 'test@test.com', 'dsaqw_123', '5465757678678', '89868838686', 19, 'Lahore'),
+(8, 'Muneeb', 'muneeb', 'test1@test.com', 'okMuneeb', '3123233232453', '12345678912', 19, 'Lahore'),
+(9, 'Muneeb', 'muneebzaiodi', 'muneebzaidi@gmail.com', 'okMuneeb', '3892389283123', '03189464829', 19, 'Lahore');
 
 --
 -- Indexes for dumped tables
@@ -81,7 +76,7 @@ INSERT INTO `user` (`id`, `Name`, `Username`, `Email`, `Password`, `CNIC`, `Phon
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`,`Email`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -97,13 +92,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
