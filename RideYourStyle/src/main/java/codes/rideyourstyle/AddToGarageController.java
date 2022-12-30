f9922853969efa6a4fc91d7ee4e77431eead2584
MMuneebZaidi <119360402+MMuneebZaidi@users.noreply.github.com>
2022-12-30 14:37:15 +0500

    Logged in id fix

--- RideYourStyle/src/main/java/codes/rideyourstyle/AddToGarageController.java
+++ RideYourStyle/src/main/java/codes/rideyourstyle/AddToGarageController.java
@@ -112,52 +112,6 @@ public class AddToGarageController implements Initializable {
     }
     @FXML
     void Checkout(){
-
-//        LoginDatabaseConnection db = new LoginDatabaseConnection();
-//        System.out.println(UserLoginController.loggedIn.id);
-//        for(Vehicle vehicle : Garage.cars){
-//            try {
-//            Connection connectDB = db.getDatabaseLink();
-//            Statement stm = connectDB.createStatement();
-//            String query = "SELECT car1, car2 , car3 , car4 , car5 FROM garage WHERE user_id = '"+UserLoginController.loggedIn.id+"'";
-//            ResultSet output = stm.executeQuery(query);
-//
-//            while (output.next()){
-//                if(output.getString("car1") == null){
-//                    String q = "UPDATE `garage` SET car1 = '"+vehicle.name+"' WHERE user_id = '"+UserLoginController.loggedIn.id+"'";
-//                    Statement st1 = connectDB.createStatement();
-//                    st1.execute(q);
-//                    break;
-//                }
-//                else if(output.getString("car2") == null){
-//                    String q = "UPDATE `garage` SET car2 = '"+vehicle.name+"'WHERE user_id = '"+UserLoginController.loggedIn.id+"'";
-//                    Statement st2 = connectDB.createStatement();
-//                    st2.execute(q);
-//                    break;
-//                }
-//                else if(output.getString("car3") == null){
-//                    String q = "UPDATE `garage` SET car3 = '"+vehicle.name+"'WHERE user_id = '"+UserLoginController.loggedIn.id+"'";
-//                    Statement st3 = connectDB.createStatement();
-//                    st3.execute(q);
-//                    break;
-//                }
-//                else if(output.getString("car4") == null){
-//                    String q = "UPDATE `garage` SET car4 = '"+vehicle.name+"'WHERE user_id = '"+UserLoginController.loggedIn.id+"'";
-//                    Statement st4 = connectDB.createStatement();
-//                    st4.execute(q);
-//                    break;
-//                } else if(output.getString("car5") == null){
-//                    String q = "UPDATE `garage` SET car5 = '"+vehicle.name+"'WHERE user_id = '"+UserLoginController.loggedIn.id+"'";
-//                    Statement st5 = connectDB.createStatement();
-//                    st5.execute(q);
-//                    break;
-//                }
-//            }
-//            }catch (Exception e) {
-//            Logger.getLogger(FindCarController.class.getName()).log(Level.SEVERE, null, e);
-//            }
-//        }
-
         Garage.cars.clear();
         addToGarage.refresh();
     }
