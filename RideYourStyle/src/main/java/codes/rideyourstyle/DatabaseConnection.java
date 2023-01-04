6e23e537240731744feea015256dee02b4453292
Yan309 <FA21-BCS-105@cuilahore.edu.pk>
2023-01-04 20:14:40 +0500

    Report not done

--- RideYourStyle/src/main/java/codes/rideyourstyle/DatabaseConnection.java
+++ RideYourStyle/src/main/java/codes/rideyourstyle/DatabaseConnection.java
@@ -6,9 +6,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DatabaseConnection {
-    public static Connection DatabaseLink;
+    public Connection DatabaseLink;
 
-    public static Connection getDatabaseLink() {
+    public Connection getDatabaseLink() {
         String databaseName = "rideyourstyle";
         String databaseUser = "root";
         String databasePassword = "";
--- RideYourStyle/src/main/java/codes/rideyourstyle/JReportController.java
+++ RideYourStyle/src/main/java/codes/rideyourstyle/JReportController.java
@@ -1,16 +1,20 @@
 package codes.rideyourstyle;
 
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.design.JasperDesign;
-import net.sf.jasperreports.engine.util.JRLoader;
-import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.view.JasperViewer;
 
 import java.io.File;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,9 +22,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
@@ -30,10 +31,11 @@ public class JReportController {
     JasperDesign design;
     InputStream input = null;
     int orderno;
+    int Revenue = 0;
     @FXML
     void printReport(ActionEvent event) throws JRException {
-                HashMap parameters = new HashMap();
-                String path = "C:\\Users\\Aliyan\\JaspersoftWorkspace\\MyReports\\Order_Receipt.jrxml";
+                Map<String, Object> parameters = new HashMap<>();
+                String path = "E:\\JavaFX Codes\\SemProject\\CarIMDB\\RideYourStyle\\src\\main\\resources\\JasperReports\\Order_Receipt.jrxml";
                 jReport = JasperCompileManager.compileReport(path);
                 LoginDatabaseConnection link = new LoginDatabaseConnection();
                 Connection connectDB = link.getDatabaseLink();
@@ -42,36 +44,44 @@ public class JReportController {
                 try {
                     Statement stm = connectDB.createStatement();
                     ResultSet rs = stm.executeQuery(query);
-                    ArrayList<String> arr = new ArrayList<String>();
+                    ArrayList<String> arr = new ArrayList<>();
                     while (rs.next()){
                         orderno = rs.getInt("id");
                         String ordern = rs.getString("Listed");
                         String status = rs.getString("Status");
-                        while(status.equals("Accepted")){
-                        parameters.put("OrderNo",orderno);
-                        parameters.put("CustomerEm",UserLoginController.loggedIn.Email);
-                        parameters.put("CustomerN",UserLoginController.loggedIn.Username);
-                        parameters.put("ProductN",ordern);
-
+                        while(status.equals("Accepted")) {
+                            parameters.put("OrderNo", orderno);
+                            parameters.put("CustomerEm", UserLoginController.loggedIn.Email);
+                            parameters.put("CustomerN", UserLoginController.loggedIn.Username);
+                            parameters.put("ProductN", ordern);
                             Stream<String> List = ordern.lines();
                             List.forEach(arr::add);
-                            int Revenue=0;
-                            for(String s:arr){
-                                for(Vehicle v1:RideYouStyle.allVehicles){
-                                    if(s.equals(v1.name)){
-                                        Revenue=Revenue+Integer.valueOf(v1.price);
+                            System.out.println("ok");
+                            for (String s : arr) {
+                                for (Vehicle v1 : RideYouStyle.allVehicles) {
+                                    if (s.equals(v1.name)) {
+                                        Revenue = Revenue + Integer.parseInt(v1.price);
                                     }
                                 }
                             }
-                        parameters.put("ProductPrice",Revenue);
+                            break;
                         }
+
+                        parameters.put("ProductPrice",Revenue);
                     }
                 } catch (SQLException e) {
                     Logger.getLogger(FindCarController.class.getName()).log(Level.SEVERE, null, e);
                 }
-                Map<String, Object> params = new HashMap<>();
-                JasperPrint jasperPrint = JasperFillManager.fillReport(jReport, params, DatabaseConnection.getDatabaseLink());
-                JasperViewer jasperViewer = new JasperViewer(jasperPrint);
+//        parameters.put("OrderNo", 1);
+//        parameters.put("CustomerEm", "hehe");
+//        parameters.put("CustomerN", "ok");
+//        parameters.put("ProductN", "lel");
+//        parameters.put("ProductPrice",22);
+//        parameters.put("CustomerEm", UserLoginController.loggedIn.Email);
+                JasperPrint jasperPrint = JasperFillManager.fillReport(jReport, parameters, new JREmptyDataSource());
+               JasperExportManager.exportReportToPdfFile(jasperPrint,"jasper.pdf");
+
+                JasperViewer jasperViewer = new JasperViewer(jasperPrint, false);
                 jasperViewer.setTitle("My Jasper Report");
                 jasperViewer.setVisible(true);
     }
